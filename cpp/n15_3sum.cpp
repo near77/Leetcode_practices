c885c4e142efbe37473a77ce4cfeb6e312ff7de4
near77 <near6334@gmail.com>
2021-10-28 23:58:31 +0800

    n15

---
+++ cpp/n15_3sum.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int target2sum;
+        vector<vector<int>> answer;
+        sort(nums.begin(), nums.end());
+        int left;
+        int right;
+        for(int i = 0; i < nums.size(); i++){
+            if(i > 0){
+                if(nums[i] == nums[i-1]){
+                    continue;
+                }
+            }
+            target2sum = -nums[i];
+            left = i + 1;
+            right = nums.size() - 1;
+            while(left < right){
+                if(nums[left] + nums[right] == target2sum){
+                    vector<int> vec = {nums[i], nums[left], nums[right]};
+                    if(left - 1 > i && nums[left] == nums[left - 1]){
+                        left++;
+                        right--;
+                    }else{
+                        answer.push_back(vec);
+                        left++;
+                        right--;
+                    }
+                }else if(nums[left] + nums[right] > target2sum){
+                    right--;
+                }else{
+                    left++;
+                }
+            }
+        }
+        return answer;
+    }
+};
