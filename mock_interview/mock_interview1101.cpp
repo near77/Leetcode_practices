e499805ab11b6da5d56bf089ac295737abf04946
near77 <near6334@gmail.com>
2021-11-01 23:49:21 +0800

    mock interview 1101

---
+++ mock_interview/mock_interview1101.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+//Power of Two
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n < 0){
+            return false;
+        }else{
+            int cnt = 0;
+            while(n != 0){
+                n &= n-1;
+                cnt += 1;
+            }
+            if(cnt == 1){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+};
+//Find All Numbers Disappeared in an Array
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        int tmp;
+        for(int i = 0; i < nums.size(); i++){
+            tmp = max(-nums[i], nums[i]);
+            if(nums[tmp-1] > 0){
+                nums[tmp-1] *= -1;
+            }
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > 0){
+                ans.push_back(i+1);
+            }
+        }
+        return ans;
+    }
+};
+
+// Your interview score of 6.88/10 beats 74% of all users.
+// Time Spent: 16 minutes
+// Time Allotted: 1 hour
